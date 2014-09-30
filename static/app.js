aa3530f5d50a0b08d488873b9d6d3ee2f580e5cf
Jason Norris <jason.the.norris@gmail.com>
2014-09-30 01:34:40 -0700

    Quick fixes.

--- static/app.js
+++ static/app.js
@@ -46,30 +46,6 @@ $(function() {
     var self = this;
     var statsTableHtml = Mustache.render(this.statsTableTemplate, data);
     this.$stats.html(statsTableHtml).fadeIn();
-
-    var chartData = {
-      labels: data.wordLengthCounts.lengths,
-      datasets: [{
-        label: "My First dataset",
-        fillColor: "rgba(220,220,220,0.5)",
-        strokeColor: "rgba(220,220,220,0.8)",
-        highlightFill: "rgba(220,220,220,0.75)",
-        highlightStroke: "rgba(220,220,220,1)",
-        data: data.wordLengthCounts.counts
-      }]
-    };
-    // var chartCtx = $('.word-length-chart')[0].getContext('2d');
-    // d =    {labels: ["January", "February", "March", "April", "May", "June", "July"],
-    // datasets: [
-    //     {
-    //         label: "My First dataset",
-    //         fillColor: "rgba(220,220,220,0.5)",
-    //         strokeColor: "rgba(220,220,220,0.8)",
-    //         highlightFill: "rgba(220,220,220,0.75)",
-    //         highlightStroke: "rgba(220,220,220,1)",
-    //         data: [65, 59, 80, 81, 56, 55, 40]
-    //     }]};
-    // new Chart(chartCtx).Bar(d);
   };
 
   TextStats.prototype.toggleCalculateButton = function(isCalculating) {
